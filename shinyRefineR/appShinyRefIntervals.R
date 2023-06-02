b0e3bf03b6a6a36b8b1e8b174b2989ef1a118d46
oliverspeer <oliver.speer@startmail.com>
2023-06-02 10:50:28 +0200

    started a shiny app to choose and calculate reference intervals in clinical chemistry

---
+++ shinyRefineR/appShinyRefIntervals.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(refineR)
+
+# Define UI
+ui <- fluidPage(
+  titlePanel("Reference Interval Calculator"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("analyte", "Select Analyte:", choices = unique(Combined_Data_KC$Bezeichnung))
+    ),
+    mainPanel(
+      verbatimTextOutput("refIntervalOutput")
+    )
+  )
+)
+
+# Define server
+server <- function(input, output) {
+  output$refIntervalOutput <- renderPrint({
+    # Filter the dataframe based on the selected analyte
+    filteredData <- Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte, ]
+    
+    # Filter the filteredData to retain unique rows based on "b_Fallnummer"
+    uniqueData <- unique(filteredData, by = "b_Fallnummer")
+    
+    # Extract the values for the reference interval calculation
+    values <- uniqueData$Werte
+    
+    # Calculate the reference interval using refineR
+    refInterval <- findRI(Data = values)
+    
+    # Output the reference interval
+    paste("Reference Interval for", input$analyte, ":")
+    print(refInterval)
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
