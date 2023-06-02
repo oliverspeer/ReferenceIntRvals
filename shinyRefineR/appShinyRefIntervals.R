97e03ceaae7f762fc904a1853fc8e585b7fc04e4
oliverspeer <oliver.speer@startmail.com>
2023-06-02 18:13:16 +0200

    added filter for gender

--- shinyRefineR/appShinyRefIntervals.R
+++ shinyRefineR/appShinyRefIntervals.R
@@ -43,11 +43,16 @@ ui <- fluidPage(navbarPage(
         choices = unique(Combined_Data_KC$Bezeichnung)
       )
     ),
-    mainPanel(fluidRow(
-      column(9, verbatimTextOutput("refIntervalOutput"))
-    ),
-    fluidRow(
-      column(9, plotOutput("refIntervalPlot"))
+    mainPanel(
+      fluidRow(
+        column(4, verbatimTextOutput("refIntervalOutputBoth")),
+        column(4, verbatimTextOutput("refIntervalOutputM")),
+        column(4, verbatimTextOutput("refIntervalOutputF"))
+      ),
+      fluidRow(
+        column(4, plotOutput("refIntervalPlotBoth")),
+        column(4, plotOutput("refIntervalPlotM")),
+        column(4, plotOutput("refIntervalPlotF"))
     )))
   )
 ))
@@ -56,30 +61,56 @@ ui <- fluidPage(navbarPage(
 # Define server
 server <- function(input, output, session) {
   session$cache <- cache_mem(max_size = 4000e6)
-  refInterval <- reactive({
+  refIntervalBoth <- reactive({
     # Filter the dataframe based on the selected analyte
-    filteredData <-
-      Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte,]
-    
+    filteredData <- Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte, ]
     # Filter the filteredData to retain unique rows based on "b_Fallnummer"
-    uniqueData <-
-      filteredData[!duplicated(filteredData$b_Fallnummer), ]
-    
+    uniqueData <- filteredData[!duplicated(filteredData$b_Fallnummer),]
     # Extract the values for the reference interval calculation
     values <- uniqueData$Werte
-    
     # Calculate the reference interval using refineR
     findRI(Data = values)
   })
   
-  output$refIntervalOutput <- renderPrint({
-    # Output the reference interval
-    print(refInterval())
+  refIntervalM <- reactive({
+    filteredData <- Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte & Combined_Data_KC$f_Geschl. == "M", ]
+    uniqueData <- filteredData[!duplicated(filteredData$b_Fallnummer),]
+    values <- uniqueData$Werte
+    findRI(Data = values)
   })
   
-  output$refIntervalPlot <- renderPlot({
-    # Plot the reference interval
-    plot(refInterval())
+  refIntervalF <- reactive({
+    filteredData <- Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte & Combined_Data_KC$f_Geschl. == "F", ]
+    uniqueData <- filteredData[!duplicated(filteredData$b_Fallnummer),]
+    values <- uniqueData$Werte
+    findRI(Data = values)
+  })
+  
+  output$refIntervalOutputBoth <- renderPrint({
+    cat(input$analyte, "(all genders)\n -------------------------------------")
+    print(refIntervalBoth())
+  })
+  
+  output$refIntervalOutputM <- renderPrint({
+    cat(input$analyte, "(male)\n -------------------------------------")
+    print(refIntervalM())
+  })
+  
+  output$refIntervalOutputF <- renderPrint({
+    cat(input$analyte, "(female)\n -------------------------------------")
+    print(refIntervalF())
+  })
+  
+  output$refIntervalPlotBoth <- renderPlot({
+    plot(refIntervalBoth())
+  })
+  
+  output$refIntervalPlotM <- renderPlot({
+    plot(refIntervalM())
+  })
+  
+  output$refIntervalPlotF <- renderPlot({
+    plot(refIntervalF())
   })
 }
 
