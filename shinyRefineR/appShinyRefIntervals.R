96dc32aaf355332b15eba1dada7359bbb16ddd62
oliverspeer <oliver.speer@startmail.com>
2023-06-02 15:13:04 +0200

    added the plot

--- shinyRefineR/appShinyRefIntervals.R
+++ shinyRefineR/appShinyRefIntervals.R
@@ -1,19 +1,51 @@
 library(shiny)
 library(refineR)
+library(tidyverse)
+library(shinythemes)
+library(memoise)
+library(cachem)
+
+# set working directory ----------------------------------------------------
+#setwd("C:/R_local/labStat")
+
+
+# import rds data ---------------------------------------------------------
+
+#combined_data_kc <- readRDS("Combined_Data_KC.rds")
+
+shinyOptions(cache = cache_mem(max_size = 5000e6))
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("Reference Interval Calculator"),
+  navbarPage(
+    title = div(img(src="logo_pos.png",  
+                    height = 28, 
+                    width = 130, 
+                    style = "margin:1px 3px", "  Klinische Chemie ")
+    ), 
+    theme = shinytheme("paper"), 
+    collapsible = TRUE,
+    fluid = TRUE,
+    
+#  tabPanel  ------------------------------------------------
+    
+    tabPanel("Reference Interval Calculator", "Clinical Chemistry",
   sidebarLayout(
     sidebarPanel(
       selectInput("analyte", "Select Analyte:", choices = unique(Combined_Data_KC$Bezeichnung))
     ),
     mainPanel(
-      verbatimTextOutput("refIntervalOutput")
+      fluidRow(
+        column(6, verbatimTextOutput("refIntervalOutput")),
+        column(6, plotOutput("refIntervalPlot"))
+      )
+      
+      
+          )
+      )
     )
   )
 )
-
 # Define server
 server <- function(input, output) {
   output$refIntervalOutput <- renderPrint({
@@ -21,7 +53,7 @@ server <- function(input, output) {
     filteredData <- Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte, ]
     
     # Filter the filteredData to retain unique rows based on "b_Fallnummer"
-    uniqueData <- unique(filteredData, by = "b_Fallnummer")
+    uniqueData <- filteredData[!duplicated(filteredData$b_Fallnummer),]
     
     # Extract the values for the reference interval calculation
     values <- uniqueData$Werte
@@ -30,8 +62,25 @@ server <- function(input, output) {
     refInterval <- findRI(Data = values)
     
     # Output the reference interval
-    paste("Reference Interval for", input$analyte, ":")
+    paste0("Reference Interval for", input$analyte, ":")
     print(refInterval)
+  })
+  
+  output$refIntervalPlot <- renderPlot({
+    # Filter the dataframe based on the selected analyte
+    filteredData <- Combined_Data_KC[Combined_Data_KC$Bezeichnung == input$analyte, ]
+    
+    # Filter the filteredData to retain unique rows based on "b_Fallnummer"
+    uniqueData <- filteredData[!duplicated(filteredData$b_Fallnummer),]
+    
+    # Extract the values for the reference interval calculation
+    values <- uniqueData$Werte
+    
+    # Calculate the reference interval using refineR
+    refInterval <- findRI(Data = values)
+    
+    # Plot the reference interval
+    plot(refInterval)
   })
 }
 
